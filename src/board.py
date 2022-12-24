3c12e030bf9b5af0b3db0e6a9a8b8a0d56b59a28
atokarew <free_m1nd@mail.ru>
2022-12-24 21:39:26 +0300

    47.12

---
+++ src/board.py
@@ -0,0 +1,24 @@
+from const import *
+from square import Square
+
+class Board:
+    
+    def __init__(self) -> None:
+        self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(cols)]
+        
+        self._create()
+    
+    def _create(self):
+        for row in range(rows):
+            for col in range(cols):
+                self.squares[row][col] = Square(row, col)
+    
+    def _add_pieces(self, color):
+        pass
+    
+    
+ 
+    
+    
+    
+    
